aa46ceec986d7f100a0311e030042d45a1fc4454
K11NH0 <K11NH0@users.noreply.github.com>
2018-07-31 21:26:09 -0300

    -Interação com Usuário -Convertendo String para Int (Int.Parse)

--- Aulas/Aula3107_ConsoleApp1/Program.cs
+++ Aulas/Aula3107_ConsoleApp1/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula3107_ConsoleApp1
 {
     class Program
     {
+
+        public static int Id { get; set; }
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello WOrld!");
+            
+
+            Console.WriteLine("Digite um Numero: ");
+             Id = int.Parse(Console.ReadLine()) ;
+
+            Console.WriteLine("A Id que você digitou foi: " + Id);
+
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
